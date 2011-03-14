bb26651f0e6fef07f2f624dada115e5ccc05cbec
dvdrtrgn <dvdrtrgn@gmail.com>
2011-03-14 00:07:55 -0500

    Misc
    Split out helper.js
    Retool calculation and objects
    Trim size

--- script.js
+++ script.js
@@ -1,121 +1,104 @@
 // sanitize the github 404 ... uses jquery anyway
 
-if (typeof console == "undefined") (console = {}).log = $.noop
-
-var clog = function(){
-    console.log.apply(console,arguments);
-}
-
 var Parallax = {
-    err: {},
-    cat: {},
-    car: {},
-    bld: {},
-    pct: {},
-    cf : {
-        w   : 940,
-        h   : 375,
-        facX: 2,
-        facY: 1,
-        facZ: 5,
-        radi: 4,
-        unit:'px',
-        para: 4
+    cf: {
+        w: 940,
+        h: 375,
+        blk: 0,         /// event skipper
+        dbg: 0,         /// debug lvl
+        evt: 0,         /// events accrued
+        facX: 2,        /// multipliers x
+        facY: 1,        /// multipliers y
+        facZ: 5,        /// multiplier both
+        radi: 4,        /// "pivot" point
+        unit: 'px',
     },
-    init: function () {
-        var tmp
-        ,   cf = this.cf;
+    img: $(),
+    pct: {},
+    init: function() {
+        var tmp, me = Parallax,
+            cf = me.cf;
 
-        this.parallax = _gEid('Parallax');
-        this.backgrnd = _gEid('Backgr0');
-        this.bld._one = _gEid('Build_1');
-        this.bld._two = _gEid('Build_2');
+        _origins('parallax', '#Parallax');
+        _origins('backgrnd', '#Backgr0', [7, 8]);
+        _origins('bld._one', '#Build_1', [15, 4]);
+        _origins('bld._two', '#Build_2', [25, 8]);
+        _origins('car.shad', '#CarShad', [7, 0]);
+        _origins('car.body', '#CarBody', [6, 0]);
+        _origins('cat.shad', '#CatShad', [2, -1]);
+        _origins('cat.body', '#CatBody', [1, -2]);
+        _origins('err.mess', '#Err_msg', [4, -1]);
 
-        this.car.shad = _gEid('CarShad');
-        this.car.body = _gEid('CarBody');
-        this.cat.shad = _gEid('CatShad');
-        this.cat.body = _gEid('CatBody');
-
-        this.err.mess = _gEid('Err_msg');
-
-        tmp = this.parallax;
-        tmp.addEventListener('mousedown', function(evt){
+        tmp = me.parallax;
+        tmp.addEventListener('mousedown', function(evt) {
             evt.preventDefault();
         }, false);
-        $(tmp).bind('mousemove', this.move);
+        $(tmp).bind('mousemove', me.move);
 
-        cf.off = tmp = _position(tmp);
+        function _origins(str, id, rng) {
+            var tmp, cf = me.cf,
+                img = me.img,
+                obj = me[str] = $(id).get(0);
 
-        _origins(this.parallax, tmp, [7, 0]);
-        _origins(this.backgrnd, tmp, [7, 8]);
-        _origins(this.bld._one, tmp, [15,4]);
-        _origins(this.bld._two, tmp, [25,8]);
+            if (rng) me.img = img.add(obj);
+            cf.box = cf.box || _position(obj);
 
-        _origins(this.car.shad, tmp, [11, 0]);
-        _origins(this.car.body, tmp, [9, -1]);
-        _origins(this.cat.shad, tmp, [0, -1]);
-        _origins(this.cat.body, tmp, [1, -2]);
-
-        _origins(this.err.mess, tmp, [4, -1]);
-
-        function _origins(obj, off, rng) {
             try {
-                var tmp = _position(obj);
+                tmp = _position(obj);
                 obj.o = {
-                    x : tmp.x - off.x,
-                    y : tmp.y - off.y
+                    x: tmp.x - cf.box.x,
+                    y: tmp.y - cf.box.y
                 };
                 obj.r = {
-                    x : (rng[0] - cf.para) * cf.facZ * cf.facX,
-                    y : (cf.para - rng[1]) * cf.facZ * cf.facY
+                    x: (rng[0] - cf.radi) * cf.facZ * cf.facX,
+                    y: (cf.radi - rng[1]) * cf.facZ * cf.facY
                 };
-                clog('tmp',tmp.x,tmp.y,'off',off.x,off.y,'objo',obj.o.x,obj.o.y,'objr',obj.r.x,obj.r.y,obj)
-            } catch (err){
-                clog(err);
+                if (me.cf.dbg > 1)
+                    clog('pos',  [tmp.x, tmp.y],
+                         'off',  [obj.o.x, obj.o.y],
+                         'play', [obj.r.x, obj.r.y],obj);
+            } catch(err) {
+                clog('_origins', err);
             }
         }
+
         function _position(obj) {
             var tmp = {
                 x: 0,
                 y: 0
             };
-            do tmp.x += obj.offsetLeft, tmp.y += obj.offsetTop;
+            do tmp.x += obj.offsetLeft,
+            tmp.y += obj.offsetTop;
             while (obj = obj.offsetParent);
             return tmp;
         }
-        function _gEid(e){
-            return document.getElementById(e)
-        }
     },
-    move : function (evt) {  // event handler ... this == element
+    move: function(evt) {   /// event handler ... this == element
         var me = Parallax;
-        me.calcPct(evt);     // Place items along their range
-        givePos(me.backgrnd);
-        givePos(me.bld._one);
-        givePos(me.bld._two);
-        givePos(me.car.shad);
-        givePos(me.car.body);
-        givePos(me.cat.shad);
-        givePos(me.cat.body);
-        givePos(me.err.mess);
+        if (me.cf.blk) return me.cf.dbg > 2 ? clog('block') : null;
+        else me.cf.blk = 1; /// start blocking
+        setTimeout(function() {
+            me.cf.blk = 0
+        }, 15);
+        me.calc(evt);       /// Place items along their range
+        me.img.each(givePos);
         return true;
 
         function givePos(obj) {
-            var tmp = me.cf.unit;
-            obj.style.left = ((obj.o.x - obj.r.x/2) + (obj.r.x * me.pct.x)) + tmp;
-            obj.style.top  = ((obj.o.y + obj.r.y/2) - (obj.r.y * me.pct.y)) + tmp;
+            obj = this;
+            obj.style.left = ((obj.o.x - obj.r.x / 2) + (obj.r.x * me.pct.x)) + me.cf.unit;
+            obj.style.top = ((obj.o.y + obj.r.y / 2) - (obj.r.y * me.pct.y)) + me.cf.unit;
+            return (me.cf.dbg > 1) ? clog(me.cf.evt += .125) : null;
         }
     },
-    calcPct: function (evt){
-        var tmp = this.parallax.o
-        ,   x = evt.pageX - this.cf.off.x
-        ,   y = evt.pageY - this.cf.off.y;
-        this.pct.x = .01 * Math.round(((x - tmp.x) / this.cf.w) * 100);
-        this.pct.y = .01 * Math.round(((y - tmp.y) / this.cf.h) * 100);
-        clog([x,this.pct.x],[y,this.pct.y]);
+    calc: function(evt) {
+        var me = Parallax,
+            x = evt.pageX - me.cf.box.x,
+            y = evt.pageY - me.cf.box.y;
+        me.pct.x = (x - me.parallax.o.x) / me.cf.w;
+        me.pct.y = (y - me.parallax.o.y) / me.cf.h;
+        return (me.cf.dbg) ? clog(me.cf.evt++, [x, y], [me.pct.x, me.pct.y]) : null;
     }
 };
 
-$(function(){
-    Parallax.init();
-});
+$(Parallax.init);
